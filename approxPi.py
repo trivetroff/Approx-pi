de359f8a37af4f7f73ce6f2b5852242402f0b871
trivetroff <tristan@vetroff.fr>
2019-01-28 15:46:17 +0100

    Classic and Imparis methods are implemented, with comparaison graph display and compare function

--- approxPi.py
+++ approxPi.py
@@ -1,1 +1,74 @@
+import argparse
+import math
 
+from src import myClass
+from src.myClass import PiMethodes
+from src.myClass import Graph
+from src.myClass import Data
+
+
+#Tools functions:
+def roundNumber(number, precision):
+    return math.floor(number * (math.pow(10, precision))) / (math.pow(10, precision))
+
+
+#Main functions
+def gen_pi(args):
+    engine = PiMethodes()
+    if args.method == '1':
+        print(engine.MethodeSerieInvCarres(args.depth))
+    elif args.method == '2':
+        print (engine.MethodeSerieInvCarresImparis(args.depth))
+
+def displayGraphCompareMethods(args):
+    engine = PiMethodes()
+    view = Graph()
+    result_invCarres = []
+    result_invCarresImparis = []
+    for k in range (0, args.depth):
+        result_invCarres.append(abs(engine.MethodeSerieInvCarres(k) - engine.realPi()))
+        result_invCarresImparis.append(abs(engine.MethodeSerieInvCarresImparis(k) - engine.realPi()))
+    view.addData(Data(result_invCarres, label='MethodeSerieInvCarres')) \
+        .addData(Data(result_invCarresImparis, label='MethodeSerieInvCarresImparis')) \
+        .showLegend() \
+        .view()
+
+def estimatePi(args):
+    engine = PiMethodes()
+    pi = engine.realPi()
+    pi = roundNumber(pi, args.precision)
+    estimation = 0
+    depth = 1
+    while not roundNumber(estimation, args.precision) == pi:
+        if (args.method == '1'):
+            estimation = engine.MethodeSerieInvCarres(depth)
+        elif (args.method == '2'):
+            estimation = engine.MethodeSerieInvCarresImparis(depth)
+        depth += 1
+    print("Result :" + str(depth))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='cmd')
+    subparser.required = True
+
+    #Generation of pi parse
+    genpi_pars = subparser.add_parser('genpi', help='Generate pi')
+    genpi_pars.add_argument("--method", help="Choose the method (normal(1), imparis(2))", type=str, default='1')
+    genpi_pars.add_argument("depth", help="Depth of sum", type=int)
+    genpi_pars.set_defaults(func=gen_pi)
+
+    #Generation of comparative graph
+    compGraph_pars = subparser.add_parser('compgraph', help='Generate pi')
+    compGraph_pars.add_argument("depth", help="Depth of sum", type=int)
+    compGraph_pars.set_defaults(func=displayGraphCompareMethods)
+
+    #Generation of findpi parse
+    findpi_pars = subparser.add_parser('estimatepi', help='Find index for which pi is estimated with a good precision')
+    findpi_pars.add_argument("precision", help="Precision of pi", type=int)
+    findpi_pars.add_argument("--method", help="Choose the method (normal(1), imparis(2))", type=str, default='1')
+    findpi_pars.set_defaults(func=estimatePi)
+
+    args = parser.parse_args()
+    print(args)
+    args.func(args)
---
+++ src/myClass.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+import matplotlib.pyplot as plt
+import math
+
+
+
+class Data:
+    def __init__(self, data, label=None):
+        self.data = data
+        self.label = label
+
+class View:
+    def __init__(self):
+        self.dataList=[]
+
+    def addData(self, data):
+        self.dataList.append(data)
+        return self
+
+    @abstractmethod
+    def view(self):
+        pass
+
+
+class Graph(View, ABC):
+    def __init__(self):
+        super(Graph, self).__init__()
+        self.legend = False
+
+    def showLegend(self):
+        #enable display for the legend
+        self.legend = True
+        return self
+
+    def view(self):
+        #display the Graph
+        for data in self.dataList:
+            plt.plot(data.data, label='{}'.format(data.label))
+        if self.legend:
+            plt.legend()
+        plt.show()
+
+
+class PiMethodes:
+    def __init__(self):
+        self.value = 0
+
+    def SerieInvCarres(self, n):
+        if (n<0):
+            raise ValueError('N should be positive')
+        result = 0
+        for k in range(1, n+1):
+            result += 1/math.pow(k, 2)
+        return result
+
+    def MethodeSerieInvCarres(self, n):
+        return math.sqrt(6 * self.SerieInvCarres(n))
+
+    def SerieInvCarresImparis(self, n):
+        if (n < 0):
+            raise ValueError('N should be positie')
+        result = 0
+        for k in range(0, n+1):
+            result += 1 / math.pow((2 * k) + 1, 2)
+        return result
+
+    def MethodeSerieInvCarresImparis(self,n):
+        return math.sqrt(8 * self.SerieInvCarresImparis(n))
+
+    def realPi(self):
+        return math.pi
